ace8f0fdefbd11ec754be9078446e92de28b42a4
Oren Zbeda <oren.zbeda@gmail.com>
2020-11-19 16:44:00 +0200

    zzz

--- azState.js
+++ azState.js
@@ -10,17 +10,19 @@ class azWatcher  {
     startWatch(watchObj,delay=true){
         let root = Symbol("root");
         this.__proxiex[root]={};
+        this.__timeout[root]={};
         //return this.watchFactory(watchObj,cb,'root',root,delay);
-        return this.watchFactory(watchObj,(p)=>{this.topLevelCb(p)},'root',root,delay);
+        return this.watchFactory(watchObj,'root',root,delay);
         
     }
     
-    watchFactory(watchObj,cb,path,root,delay){
+    watchFactory(watchObj,path,root,delay){
         
         let validator={
             set: (obj, prop, value)=> {        
                 obj[prop] = value;
-                this.delaydb(()=>{cb(`${path}.${prop}`)},delay,root)
+                //this.delaydb(()=>{cb(`${path}.${prop}`)},delay,root)
+                this.topLevelCb(`${path}.${prop}`,delay,root);
                 return true;
             },
             get:(obj, prop, receiver)=> {                            
@@ -28,7 +30,7 @@ class azWatcher  {
                 if (typeof value === 'object') {                 
                     if (!this.__proxiex[root][`${path}.${prop}`]){ //so we do not duplicate proxies for each get
                         //console.log("zzz new proxy",`${path}.${prop}`)
-                        this.__proxiex[root][`${path}.${prop}`] = this.watchFactory(value,cb,`${path}.${prop}`,root,delay);
+                        this.__proxiex[root][`${path}.${prop}`] = this.watchFactory(value,`${path}.${prop}`,root,delay);
                     }
                     return this.__proxiex[root][`${path}.${prop}`];                        
                 }
@@ -36,7 +38,8 @@ class azWatcher  {
             },
             deleteProperty: (obj, prop)=> {                                 
                 delete obj[prop];
-                this.delaydb(()=>{cb(`${path}.${prop}`)},delay,root)
+                //this.delaydb(()=>{cb(`${path}.${prop}`)},delay,root)
+                this.topLevelCb(`${path}.${prop}`,delay,root);
                 return true;            
             }
         }
@@ -44,27 +47,39 @@ class azWatcher  {
         return new Proxy(watchObj, validator);
     }
     
-    delaydb(cb,delay,root){        
+    delaydb(cb,delay,root,path){        
         if (!delay){cb();return;}
 
-        if (this.__timeout[root]){
-            clearTimeout(this.__timeout[root]);            
+        if (this.__timeout[root][path]){
+            clearTimeout(this.__timeout[root][path]);            
         }
 
-        this.__timeout[root] = setTimeout(()=>{
-            delete this.__timeout[root];
+        this.__timeout[root][path] = setTimeout(()=>{
+            delete this.__timeout[root][path];
             cb();
         },50)
     }
     
-    topLevelCb(path){
+    topLevelCb(path,delay,root){
         let pathArr = path.split('.');
         let curPath='';
         for (let rec of pathArr){
             curPath+=rec;
-            if (this.watchCBs[curPath]){
-                for (let cb of this.watchCBs[curPath]){
-                    cb(path)
+            if (this.watchCBs[curPath] && this.watchCBs[curPath].func){
+                this.watchCBs[curPath].dirtyPath=this.watchCBs[curPath].dirtyPath || {};
+                this.watchCBs[curPath].dirtyPath[path]=true;
+
+                for (let cb of this.watchCBs[curPath].func){
+                    if (!delay){
+                        cb(path)
+                    }else{
+                        let closureF = ((curPath,cb)=>{
+                            return ()=>{cb(this.watchCBs[curPath].dirtyPath);this.watchCBs[curPath].dirtyPath=[]}
+                        })(curPath,cb)
+                        
+                        this.delaydb(closureF,delay,root,curPath)
+                    }
+                    
                 }
             }
             curPath+=".";
@@ -73,10 +88,11 @@ class azWatcher  {
     
     addWatch(path,cb){
         path='root'+(path? `.${path}` : ''); 
-        if (this.watchCBs[path]){
-            this.watchCBs[path].push(cb);
+        this.watchCBs[path]=this.watchCBs[path] || {};
+        if (this.watchCBs[path].func ){
+            this.watchCBs[path].func.push(cb);
         }else{
-            this.watchCBs[path]=[cb];
+            this.watchCBs[path].func=[cb];
         }
     }
 }
