00b100f33a1d068a798390b35cddbc4309fd7456
Oren Zbeda <oren.zbeda@gmail.com>
2021-07-22 15:38:03 +0300

    zz

--- azState.js
+++ azState.js
@@ -1,18 +1,18 @@
-!(()=>{    
-	let serviceRegistered = false;
-	if ('serviceWorker' in navigator) {
-	//  window.addEventListener('load', function() {
-	    navigator.serviceWorker.register('/azStareService.js').then(function(registration) {
-	      // Registration was successful
-	      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-	      serviceRegistered=true;
-	    }, function(err) {
-	      // registration failed :(
-	      console.log('ServiceWorker registration failed: ', err);
-	    });
-//	  });
-    }
-})
+// !(()=>{    
+// 	let serviceRegistered = false;
+// 	if ('serviceWorker' in navigator) {
+// 	//  window.addEventListener('load', function() {
+// 	    navigator.serviceWorker.register('/azStareService.js').then(function(registration) {
+// 	      // Registration was successful
+// 	      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+// 	      serviceRegistered=true;
+// 	    }, function(err) {
+// 	      // registration failed :(
+// 	      console.log('ServiceWorker registration failed: ', err);
+// 	    });
+// //	  });
+//     }
+// })
     
 
 
